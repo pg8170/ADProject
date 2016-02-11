93c6246e016c2d7cf71df56a42ee902d218f8cba
Prashant Girennavar <giteepag@yahoo.co.in>
2016-02-11 12:24:41 +0100

    Font size change for nav bar

--- app/controllers/computer_controller.rb
+++ app/controllers/computer_controller.rb
@@ -3,13 +3,13 @@ class ComputerController < ApplicationController
 	def show
 
 	#@computers = Computer.all
-	@tasks_grid = initialize_grid(Computer)
-
+	@tasks_grid = initialize_grid(Computer,
+		enable_export_to_csv: true,
+  		csv_file_name:'All_Computers')
 	end
 
 	def expired
 	@q = Computer.ransack({ disabled_eq: 'Yes' }, { auth_object: :admin })
     @computers = initialize_grid(@q.result)
   	end
-
-end
+ end
